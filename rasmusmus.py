9ad4d9b8f1f299d375e7f14df9e39d5405ef37c9
rasmuswael <rallelind@gmail.com>
2021-04-22 10:47:55 +0200

    enviroment

--- rasmusmus.py
+++ rasmusmus.py
@@ -1,1 +1,24 @@
+import gym
+import gym_snake
 
+# Construct Environment
+env = gym.make('snake-v0')
+observation = env.reset() # Constructs an instance of the game
+
+# Controller
+game_controller = env.controller
+
+# Grid
+grid_object = game_controller.grid
+grid_pixels = grid_object.grid
+
+# Snake(s)
+snakes_array = game_controller.snakes
+snake_object1 = snakes_array[0]
+
+actions = [0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,]
+
+for i in range(1000):
+    env.render()
+    env.step(actions[i]) # take a random action
+env.close()
