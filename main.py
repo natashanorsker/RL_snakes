25b7dd88dd6ec2069ae63534af185689cb6c56a0
natashanorsker <natashanorsker@gmail.com>
2021-04-23 13:30:04 +0200

    copied agents form the course

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import irlc
+from irlc import Agent, train
 from irlc.ex09.rl_agent import TabularAgent, TabularQ
 from irlc.ex11.q_agent import QAgent
-
-
-
+import gym
+import gym_snake
 
 
 # set grid size of snake map
 # N =
 
 # Set total number of iterations
-#
+num_episodes = 10  # change this :)
+max_runs = 10
 
 # Make environment instance
-#
+env = gym.make('snake-v0')
+
+# Make Agent instance
+agent = QAgent(env, gamma=1.0, alpha=0.5)
+
+# Train Agent
+snake_exp = f"experiments/snake"
+train(env, agent, snake_exp, num_episodes=num_episodes, max_runs=max_runs)
